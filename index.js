980fc619d31dbe76775bbb87347b87102ed244b0
Chhayflyguy <chhayhighlay1232@gmail.com>
2024-09-19 12:11:00 +0700

    new

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app=express();
 
 const PORT = 8000;
 
+app.get("/", (req, res) => {
+    res.send('Welcome to the Users API! Access /users to see the user data.');
+});
+
 app.get("/users", (req, res)=>{
     return res.json(users);
-})
+});
 
 app.listen(PORT, ()=> console.log(`Server started at PORT: http://localhost:${PORT}`));
